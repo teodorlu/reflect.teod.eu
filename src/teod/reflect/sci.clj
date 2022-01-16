8e2bc56f40fa5d64cfdef2f54399f6f034d25e9f
Teodor Heggelund <git@teod.eu>
2022-01-16 21:20:34 +0100

    Managed execution?

--- src/teod/reflect/sci.clj
+++ src/teod/reflect/sci.clj
@@ -2,25 +2,14 @@
   (:refer-clojure :exclude [eval])
   (:require [sci.core :as sci]))
 
-;; consider removing - indirection might not be worth it
-;;
-;; works, though.
+;; clojure.java.shell/sh
+
 (defn link [x]
   [:a {:href x} x])
 
 (defn ctx []
   (sci/init {:namespaces {'teod {'link link}}}))
 
-;; Problem: I need to pass some kind of context to eval. That means ... I need a
-;; convention for the first arg called.
-;;
-;; Or ... attach some metadata given a keyword that I could pull out.
-;;
-;; :thinking_face:
-;;
-;; But ... that metadata map can then /not/ be attached to certain objects. So
-;; perhaps just having a config map as the first param is the best option, and
-;; just ignore it (_opts) when it's not needed.
-(defn eval [opts form]
+(defn eval [{:keys [sci-env]} form]
   (sci/eval-form (ctx)
                  form))
