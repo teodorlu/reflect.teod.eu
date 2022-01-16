eb275a7a4dcb79c8182cc1071854f6111c59b21e
Teodor Heggelund <git@teod.eu>
2022-01-16 22:30:15 +0100

    Article LOL

--- src/teod/reflect/sci.clj
+++ src/teod/reflect/sci.clj
@@ -7,13 +7,24 @@
 (defn link [x]
   [:a {:href x} x])
 
+(defn ^:private map-vals [f m]
+  (into {}
+        (map (fn [[k v]]
+               [k (f v)])
+             m)))
+
+(let [{:keys [sci-env]} (quote {:sci-env {teod {slurp clojure.core/slurp
+                                                sh clojure.java.shell/sh}}})]
+  (map-vals (fn [m]
+              (map-vals requiring-resolve m))
+            sci-env))
+
 (defn ctx [env]
-  (sci/init {:namespaces {'user env}}))
+  (sci/init {:namespaces env}))
 
 (defn eval [{:keys [sci-env]} form]
-  (let [env (reduce-kv (fn [coll k v]
-                             (assoc coll k (requiring-resolve v)))
-                           {}
-                           sci-env)]
+  (let [env (map-vals (fn [m]
+                        (map-vals requiring-resolve m))
+                      sci-env)]
     (sci/eval-form (ctx env)
                    form)))
