9274564dee60ce28de9430b4d316cb081b6f2dd2
Teodor Heggelund <git@teod.eu>
2022-01-17 01:04:34 +0100

    Actually do the test

--- src/teod/reflect/pandoc.clj
+++ src/teod/reflect/pandoc.clj
@@ -3,7 +3,7 @@
   (:require
    [clojure.data.json :as json]
    [clojure.java.shell :refer [sh]]
-   [clojure.walk :refer [postwalk]]
+   [clojure.walk :refer [prewalk postwalk]]
    [clojure.string :as str]))
 
 (defn -parse [{:keys [source format]}]
@@ -34,16 +34,49 @@
        (let [level (get (:c el) 0)]
          (<= 1 level 6))))
 
-(comment
-  ;; a valid header:
-  (header? {:t "Header", :c [1 ["header" [] []] ["header"]]}))
+(defn ->hiccup* [form opts]
+  (cond (= (:t form)
+           "Para")
+        (into [:p]
+              (map #(->h* % opts) (:c form)))
+
+        (= (:t form)
+           "Str")
+        (:c form)
+
+        (= (:t form)
+           "Space")
+        " "
+
+        ;; otherwise -- don't return!
+        :else
+        nil))
+
+(defn ->hiccup2
+  ([data] (->hiccup2 data {}))
+  ([data opts]
+   (into [:div]
+         (for [p data]
+           (->hiccup* p opts)))))
+
+(->hiccup2 [{:t "Para", :c [{:t "Str", :c "Paragraph"}]}])
+
+(->hiccup2
+ [{:t "Para", :c [{:t "Str", :c "Para"} {:t "Space"} {:t "Str", :c "1."}]}
+  {:t "Para",
+   :c
+   [{:t "Str", :c "Para"}
+    {:t "Space"}
+    {:t "Emph", :c [{:t "Str", :c "two"}]}
+    {:t "Str", :c "."}]}]
+ )
 
 (defn ->hiccup
   ([data]
    (->hiccup data {}))
   ([data {:keys [debug]}]
    (into [:div]
-         (postwalk (fn [el]
+         (prewalk (fn [el]
                      (cond
                        (= "Str" (:t el))
                        (:c el)
@@ -75,45 +108,8 @@
 
                        :else
                        el))
-                   data))
-   ))
+                   data))))
 
-(comment
-  (org->hiccup "/Use case: interactively provide, explore/")
-
-  (org->hiccup "/Use case: interactively provide, explore/" {:debug true})
-
-  (org-> "/My emphasis/")
-  [{:t "Para", :c [{:t "Emph", :c [{:t "Str", :c "My"} {:t "Space"} {:t "Str", :c "emphasis"}]}]}]
-
-  (org->hiccup "/My emphasis/" {:debug true})
-  ;; => [:div [:p [{:t "Emph", :c ["My" " " "emphasis"]}]]]
-  [:div [:p [:em "My emphasis"]]]
-
-
-
-  ;; without Emph condition
-  [:div [:p [{:t "Emph", :c ["Use" " " "case:" " " "interactively" " " "provide," " " "explore"]}]]]
-
-  ;; with Emph condition
-  [:div [:p [[:em ["Use" " " "case:" " " "interactively" " " "provide," " " "explore"]]]]])
-
-
-
-(comment
-  (-> "* header\nBody goes here"
-      org->
-      ->hiccup)
-
-  (org-> "* head /more/")
-  (-> "- item 1\n- item 2"
-      org->
-      ->hiccup)
-  (-> "some text"
-      org->
-      ->hiccup)
-  )
-;; => nil
 (defn org->hiccup
   ([data]
    (org->hiccup data {}))
@@ -128,112 +124,3 @@
   ([data opts]
    (-> data markdown-> ->hiccup)))
 
-(comment
-
-  (org-> "* my header")
-  ;; => [{:t "Header",
-  ;;      :c
-  ;;      [1
-  ;;       ["my-header" [] []]
-  ;;       [{:t "Str", :c "my"} {:t "Space"} {:t "Str", :c "header"}]]}]
-
-  (org-> "** my header")
-  ;; => [{:t "Header",
-  ;;      :c
-  ;;      [2
-  ;;       ["my-header" [] []]
-  ;;       [{:t "Str", :c "my"} {:t "Space"} {:t "Str", :c "header"}]]}]
-  (org-> "*** my header")
-  ;; => [{:t "Header",
-  ;;      :c
-  ;;      [3
-  ;;       ["my-header" [] []]
-  ;;       [{:t "Str", :c "my"} {:t "Space"} {:t "Str", :c "header"}]]}]
-  )
-
-(comment
-  (assert (= (org-> "* head
-** subhead
-Hello!")
-  (markdown-> "# head
-## subhead
-Hello!")))
-  )
-
-
-(comment
-  ;; big example
-
-  ;; org
-  ;;
-  ;; * okay
-  ;;
-  ;; - are you okay?
-  ;; - anything else?
-
-  ;; from markdown
-  (-parse {:source "# okay
-
-- are you okay?
-- anything else?"
-           :format "markdown"})
-
-  ;; from org
-  (-parse {:source "* okay
-
-- are you okay?
-- anything else?"
-         :format "org"})
-
-  ;; into pandoc
-  [{:t "Header", :c [1 ["okay" [] []] [{:t "Str", :c "okay"}]]}
-   {:t "BulletList",
-    :c
-    [[{:t "Plain",
-       :c
-       [{:t "Str", :c "are"}
-        {:t "Space"}
-        {:t "Str", :c "you"}
-        {:t "Space"}
-        {:t "Str", :c "okay?"}]}]
-     [{:t "Plain",
-       :c [{:t "Str", :c "anything"} {:t "Space"} {:t "Str", :c "else?"}]}]]}]
-
-  (org->hiccup "* okay
-
-- are you okay?
-- anything else?")
-
-  ;; into hiccup
-  [:div
-   [:h1 "okay"]
-   [:ul
-    [:ol "are you okay?"]
-    [:ol "anything else?"]]]
-
-  (org->hiccup "
-#+TITLE: Balancing outcome orientation and bottom-up play
-
-* Outcome orientation distributes intent to teams
-")
-
-  (org-> "#+TITLE: Balancing\n\n* Outcome orientation")
-
-  (let [source "#+TITLE: Balancing outcome and process\n\n* Outcome orientation"
-        format "org"]
-    (-> (sh "pandoc" "-f" format "-t" "json" :in source)
-        :out
-        (json/read-str :key-fn keyword)
-        :meta))
-  ;; => {:title
-  ;;     {:t "MetaInlines",
-  ;;      :c
-  ;;      [{:t "Str", :c "Balancing"}
-  ;;       {:t "Space"}
-  ;;       {:t "Str", :c "outcome"}
-  ;;       {:t "Space"}
-  ;;       {:t "Str", :c "and"}
-  ;;       {:t "Space"}
-  ;;       {:t "Str", :c "process"}]}}
-
-  )
--- test/teod/reflect/pandoc_test.clj
+++ test/teod/reflect/pandoc_test.clj
@@ -2,11 +2,11 @@
   (:require [teod.reflect.pandoc :as sut]
             [clojure.test :as t]))
 
-(sut/org->hiccup "Paragraph")
-
 (t/deftest org->hiccup-test
-  (t/is (= (sut/org->hiccup "Par")
-           [:div [:p "Par"]]))
-  (t/is (= (sut/org->hiccup "Par 1\n\nPar 2")
-           [:div [:p "Par 1"] [:p "Par 2"]]))
-  )
+  (t/are [pandoc hiccup] (= (sut/->hiccup* pandoc {})
+                            hiccup)
+    {:t "Para", :c [{:t "Str", :c "Paragraph"}]}
+    [:p "Paragraph"]
+
+    {:t "Space"}
+    " "))
